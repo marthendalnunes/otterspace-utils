073a0a323fe5f37a660adc319ef47480f93a97d0
Vitor <marthendalnunes@gmail.com>
2023-02-01 23:46:19 -0300

    chore: remove commented code

--- src/pages/raft/transfer.tsx
+++ src/pages/raft/transfer.tsx
@@ -2,14 +2,6 @@ import Head from 'next/head'
 import { TransferRaftView } from '@/components/views/TransferRaftView'
 
 export default function RaftPage() {
-  // const { address } = useAccount()
-  // const { chain } = useNetwork()
-  // const chainId = chain?.id
-  // const { data, isLoading, isSuccess } = useQuery({
-  //   queryKey: ['user-rafts', address, chain?.id],
-  //   queryFn: async () => getUserRafts(address, chainId)
-  // })
-
   return (
     <>
       <Head>
