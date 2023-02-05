f6254438f98f224d0a7837f91063f08624f28ab2
Vitor <marthendalnunes@gmail.com>
2023-02-05 16:14:45 -0300

    fix: add x margin to base dialog

--- src/components/dialogs/BaseDialog.tsx
+++ src/components/dialogs/BaseDialog.tsx
@@ -10,7 +10,7 @@ export const BaseDialog = ({ children, isOpen, onClose }: BaseDialogProps) => {
   return (
     <Dialog className="min-h-screen" open={isOpen} onClose={onClose}>
       <div className="fixed inset-0 flex min-h-screen items-center justify-center bg-gray-600/50">
-        <Dialog.Panel className="flex min-h-[500px] w-full max-w-md flex-col items-center justify-center rounded-lg bg-white p-8">
+        <Dialog.Panel className="mx-4 flex min-h-[500px] w-full flex-col items-center justify-center rounded-lg bg-white p-8 xs:max-w-md">
           {children}
         </Dialog.Panel>
       </div>
