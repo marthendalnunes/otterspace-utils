5d98dd20164c9021b041a7b3ff1fa8c5e8ca520d
Vitor <marthendalnunes@gmail.com>
2023-04-21 15:07:42 -0300

    feat: Add mainnet and polygon to supported wagmi chains

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,4 @@
-import { goerli, optimism } from 'wagmi/chains'
+import { goerli, optimism, mainnet, polygon } from 'wagmi/chains'
 
-export const ETH_CHAINS = [goerli, optimism]
+export const ETH_CHAINS = [mainnet, optimism, polygon, goerli]
 export const SITE_NAME = 'Otterspace Utils'
